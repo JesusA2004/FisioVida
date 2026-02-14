a2c5f82882d39a964b23c6f1653df7bf70312c37
yatshdz <shyo221058@upemor.edu.mx>
2026-02-14 15:25:45 -0600

    Login

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ Route::get('/', function () {
         return redirect()->route('dashboard');
     }
     return redirect()->route('login');
-});
+})->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
