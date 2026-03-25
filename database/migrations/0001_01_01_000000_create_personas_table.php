5c697244dd20a13eeac6bba7eda0079547953d69
JesusA2004 <arizma2004@gmail.com>
2026-03-25 13:37:33 -0600

    Fix migraciones

--- database/migrations/0001_01_01_000000_create_personas_table.php
+++ database/migrations/0001_01_01_000000_create_personas_table.php
@@ -4,9 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    
-    public function up(): void {
+return new class extends Migration
+{
+    public function up(): void
+    {
         Schema::create('personas', function (Blueprint $table) {
             $table->id();
 
@@ -32,12 +33,12 @@ return new class extends Migration {
             $table->index(['apellido_paterno'], 'idx_personas_apellido_paterno');
             $table->index(['apellido_materno'], 'idx_personas_apellido_materno');
             $table->index(['nombres'], 'idx_personas_nombres');
-            $table->index(['tipo', 'status'], 'idx_personas_tipo');
+            $table->index(['tipo', 'status'], 'idx_personas_tipo_status');
         });
     }
 
-    public function down(): void {
+    public function down(): void
+    {
         Schema::dropIfExists('personas');
     }
-
 };
