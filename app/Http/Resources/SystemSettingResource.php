f1740f8c6e44c6832cdb69dc7d5f0a1d62940f53
Jesus Arizmendi <arizma2004@gmail.com>
2026-04-28 11:33:34 -0600

    feat: agregar configuración general y módulos habilitables

---
+++ app/Http/Resources/SystemSettingResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SystemSettingResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'key' => $this->key,
+            'value' => $this->value,
+            'type' => $this->type,
+            'group' => $this->group,
+            'label' => $this->label,
+            'description' => $this->description,
+            'is_public' => $this->is_public,
+        ];
+    }
+}
